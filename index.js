be9d779014ab7efdefa2f5c5da4e043104343798
Roquevitor <roquevitor46@gmail.com>
2024-04-27 23:49:01 -0300

    atualização

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
+// Definição da classe "heroi" para representar os personagens do jogo
 class heroi {
-    
-    constructor(nome, idade, tipo, atack){
-        this.nome = nome
-        this.idade = idade
-        this.tipo = tipo
-        this.atack = atack
-
+    // Construtor da classe, que recebe nome, idade, tipo e ataque do herói
+    constructor(nome, idade, tipo, atack) {
+        // Atribuição das propriedades recebidas como parâmetros para o objeto
+        this.nome = nome; // Nome do herói
+        this.idade = idade; // Idade do herói
+        this.tipo = tipo; // Tipo do herói (ex: guerreiro, mago, monge, ninja)
+        this.atack = atack; // Ataque que o herói realiza
     }
 
+    // Método para exibir informações sobre o herói
     escrever() {
+        // Utilização de template literals para compor a mensagem
         console.log(`O ${this.tipo} ${this.nome}, com ${this.idade} anos, atacou usando ${this.atack}`);
-
     }
-
 }
 
-let heroiAtaque = new heroi("Akashi", 25,"mago", "magia");
-let heroiAtaque1 = new heroi("cleitin", 28,"guerreiro","espada");
-let heroiAtaque2 = new heroi("Junior", 15,"monge","artes marciais");
-let heroiAtaque3 = new heroi("Hun", 18,"ninja","shuriken");
+// Criação de instâncias da classe "heroi" com diferentes características
+let heroiAtaque = new heroi("Akashi", 25, "mago", "magia"); // Herói mago
+let heroiAtaque1 = new heroi("cleitin", 28, "guerreiro", "espada"); // Herói guerreiro
+let heroiAtaque2 = new heroi("Junior", 15, "monge", "artes marciais"); // Herói monge
+let heroiAtaque3 = new heroi("Hun", 18, "ninja", "shuriken"); // Herói ninja
 
-heroiAtaque.escrever();
-heroiAtaque1.escrever();
-heroiAtaque2.escrever();
-heroiAtaque3.escrever();
+// Chamada do método "escrever()" para exibir informações sobre cada herói
+heroiAtaque.escrever(); // Exibe informações sobre o herói mago
+heroiAtaque1.escrever(); // Exibe informações sobre o herói guerreiro
+heroiAtaque2.escrever(); // Exibe informações sobre o herói monge
+heroiAtaque3.escrever(); // Exibe informações sobre o herói ninja
